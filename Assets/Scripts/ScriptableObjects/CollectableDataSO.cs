3876de05c17010646cf885f549b28475bf5e40bb
FrikiLight8 <91629600+FrikiLight8@users.noreply.github.com>
2024-11-04 20:42:49 -0300

    Scriptable Object agregado al Collectable

--- Assets/Scripts/ScriptableObjects/CollectableDataSO.cs
+++ Assets/Scripts/ScriptableObjects/CollectableDataSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/PlayerData")]
+[CreateAssetMenu(fileName = "CollectableData", menuName = "ScriptableObjects/CollectableData")]
 
 public class CollectableDataSO : ScriptableObject
 {
